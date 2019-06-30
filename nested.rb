a78bd331e7a3fddb8a6ed22dd120aef2cdd95b55
Josh Nho <joshnho@gmail.com>
2019-06-30 02:16:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto] = "Josh"
+programmer_hash[:yukihiro_matsumoto] = ""
 programmer_hash
     
 end
